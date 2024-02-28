608fdadc3607a9ba5ab21256067aa519624f411c
Mathias Toftås <mathias.ortenberg-toftas@semcon.com>
2024-02-28 15:43:19 +0100

    more tests

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,7 +2,7 @@ import time
 
 import pytest
 
-from as_async import as_async, as_async_generator
+from as_async import as_async, as_async_generator, to_async, to_async_generator
 
 
 @pytest.mark.asyncio
@@ -18,6 +18,46 @@ async def test_as_async():
     b = buz()
     tmp.append("foo")
     await b
+
+    assert tmp == ["foo", "buz"]
+
+
+@pytest.mark.asyncio
+async def test_to_async():
+    tmp = []
+
+    def buz():
+        """my cool function that blocks the event loop for 0.1 seconds."""
+        time.sleep(0.1)
+        tmp.append("buz")
+
+    a_buz = to_async(buz)
+
+    b = a_buz()
+    tmp.append("foo")
+    await b
+
+    assert tmp == ["foo", "buz"]
+
+
+@pytest.mark.asyncio
+async def test_to_async_generator():
+    tmp = []
+
+    def buz():
+        """my cool generator that blocks the event loop for 0.1 seconds.
+
+        Yields:
+            str: "buz"
+        """
+        time.sleep(0.1)
+        tmp.append("buz")
+        yield "buz"
+
+    a_gen = to_async_generator(buz)()
+    tmp.append("foo")
+    async for _ in a_gen:
+        pass
 
     assert tmp == ["foo", "buz"]
 
